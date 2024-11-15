509f4fac5bf0d3ba369e60ef9c5860bb0ce42e62
EvaBr <eva.breznik91@gmail.com>
2024-11-15 13:02:57 +0000

    started working on the other topolosses.

--- nnunetv2/training/nnUNetTrainer/variants/loss/EvasTrainers.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/EvasTrainers.py
@@ -165,7 +165,7 @@ class nnUNetTrainer_150epochs(nnUNetTrainer):
         super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
         self.num_epochs = 150
         
-class nnUNetTrainer_250epochs(nnUNetTrainer):
+class nnUNetTrainer_300epochs(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
                  device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
